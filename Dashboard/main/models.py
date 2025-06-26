257336fde6fe1812aac74cebaa0621a39120597f
dbenoitpicard28 <dbenoitpicard@gmail.com>
2025-06-26 19:50:00 +0200

    Session 6

--- Dashboard/main/models.py
+++ Dashboard/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class DashboardStat(models.Model):
+    users = models.IntegerField()
+    revenue = models.DecimalField(max_digits=10, decimal_places=2)
+    errors = models.IntegerField()
+    session = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Transaction(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
